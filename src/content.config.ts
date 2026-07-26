7fa12698472d3201a8dfc29edd9b11562bef64fa
nicholasboyce <gh@nicholasboyce.dev>
2026-07-25 20:46:10 -0400

    fix z import

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,6 @@
 // Import utilities from `astro:content`
-import { z, defineCollection } from "astro:content";
+import { defineCollection } from "astro:content";
+import { z } from 'astro/zod'
 // Define a `type` and `schema` for each collection
 const postsCollection = defineCollection({
     type: 'content',
