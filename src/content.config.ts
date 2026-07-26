014f539d032be4dbcbf3c65992c50dabf8c674e8
nicholasboyce <gh@nicholasboyce.dev>
2026-07-25 21:13:19 -0400

    change back render call and updated definecollection

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,9 +1,11 @@
 // Import utilities from `astro:content`
 import { defineCollection } from "astro:content";
-import { z } from 'astro/zod'
+import { z } from 'astro/zod';
+import { glob } from 'astro/loaders';
+
 // Define a `type` and `schema` for each collection
 const postsCollection = defineCollection({
-    type: 'content',
+    loader: glob({ pattern: "**/*.md", base: "./src/content/posts" }),
     schema: z.object({
       title: z.string(),
       pubDate: z.date(),
